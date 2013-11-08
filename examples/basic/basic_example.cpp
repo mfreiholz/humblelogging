e4d6e5c500503f6b0c9e06e2aa3665949f0303f7
mfreiholz <manuel@insanefactory.com>
2013-11-08 20:26:58 +0100

    Adds high performance counter implementation for WIN32.

--- examples/basic/basic_example.cpp
+++ examples/basic/basic_example.cpp
@@ -21,7 +21,15 @@ long getTimestampMillis()
 #ifdef WIN32
 long getTimestampMillis()
 {
-  return 0;
+  static LARGE_INTEGER s_frequency;
+  static BOOL s_use_qpc = QueryPerformanceFrequency(&s_frequency);
+  if (s_use_qpc) {
+    LARGE_INTEGER now;
+    QueryPerformanceCounter(&now);
+    return (1000LL * now.QuadPart) / s_frequency.QuadPart;
+  } else {
+    return GetTickCount();
+  }
 }
 #endif
 
