03a697dfe211334ffb13e8792a09d66124b88988
mfreiholz <manuel@insanefactory.com>
2014-08-19 11:26:18 +0200

    Updates find script (shouldn't be used yet)

--- cmake/FindHumbleLogging.cmake
+++ cmake/FindHumbleLogging.cmake
@@ -1,15 +1,14 @@
 # Find "include" directory of installation.
-# Determined by environment variable "HumbleLogging_DIR".
 find_path(HLL_INCLUDE_DIR
   humblelogging/api.h
-  HINTS $ENV{HumbleLogging_DIR}/include
+  HINTS ../include
 )
 
 # Windows libs.
 if(WIN32)
   find_library(HLL_LIBRARY
     NAMES humblelogging
-    HINTS $ENV{HumbleLogging_DIR}/lib
+    HINTS ../lib
   )
 endif(WIN32)
 
@@ -22,5 +21,7 @@ if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
 endif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
 
 # Export variables.
-set(HUMBLE_LOGGING_INCLUDE_DIRS ${HLL_INCLUDE_DIR})
-set(HUMBLE_LOGGING_LIBRARIES optimized ${HLL_LIBRARY} debug ${HLL_LIBRARY})
+set(HumbleLogging_FOUND)
+set(HumbleLogging_INCLUDE_DIRS ${HLL_INCLUDE_DIR})
+set(HumbleLogging_LIBRARIES optimized ${HLL_LIBRARY} debug ${HLL_LIBRARY})
+set(HumbleLogging_DEFINITIONS)
